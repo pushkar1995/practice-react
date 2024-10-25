52ec98bc7f7b4c9b660dcdedd745cca3fdbf0388
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-10-25 14:51:17 +0545

    Implemented Header Groups in react table, Video 5

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -2,12 +2,12 @@
 import { useMemo } from 'react'
 import { useTable } from 'react-table'
 import MOCK_DATA from '../../MOCK_DATA.json'
-import { COLUMNS } from './columns'
+import { GROUPED_COLUMNS } from './columns'
 import './table.css'
 
 const BasicTable = () => {
 
-    const columns = useMemo(() => COLUMNS, [])
+    const columns = useMemo(() => GROUPED_COLUMNS, [])
     const data = useMemo(() => MOCK_DATA, [])
 
     const { getTableProps, getTableBodyProps, headerGroups, footerGroups, rows, prepareRow } = useTable({ 
--- src/components/columns.js
+++ src/components/columns.js
@@ -1,7 +1,8 @@
 export const COLUMNS = [   
     { 
         Header: 'Id',
-        accessor: 'id'
+        accessor: 'id',
+        Footer: 'Id'
     },
     { 
         Header: 'First Name',
@@ -29,4 +30,50 @@ export const COLUMNS = [
         Footer: 'Phone'
     },
    
+]
+
+export const GROUPED_COLUMNS = [   
+    { 
+        Header: 'Id',
+        accessor: 'id',
+        Footer: 'Id'
+    },
+    { 
+        Header: 'Name',
+        Footer: 'Name',
+        columns: [ 
+            { 
+                Header: 'First Name',
+                accessor: 'first_name',
+                Footer: 'First Name'
+            },
+            { 
+                Header: 'Last Name',
+                accessor: 'last_name',
+                Footer: 'Last Name'
+            },
+        ]
+    },
+    { 
+        Header: 'Info',
+        Footer: 'Info',
+        columns: [ 
+            { 
+                Header: 'Date of Birth',
+                accessor: 'date_of_birth',
+                Footer: 'Date of Birth'
+            },
+            { 
+                Header: 'Country',
+                accessor: 'country',
+                Footer: 'Country'
+            },
+            { 
+                Header: 'Phone',
+                accessor: 'phone',
+                Footer: 'Phone'
+            },
+        ]
+    },
+
 ]
