ebcbba5e65b32bdb5bc0c3bdde8b01fee6a5cb0e
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-11-26 17:22:23 +0545

    Implemented useState Type Assertion

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Oscar } from './components/Oscar'
 import Button from './components/Button'
 import { Input } from './components/Input'
 import { Container } from './components/Container'
+import User from './components/state/User'
 
 function App() {
   const personName = {
@@ -50,6 +51,7 @@ function App() {
       />
       <Input value='' handleChange={(event) => console.log(event)} />
       <Container styles={{ border: '1px solid black', padding: '1rem'}} />
+      <User />
     </div>
   )
 }
