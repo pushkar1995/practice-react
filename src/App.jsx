cf839e54341a316e48fed80b59534783bba5659d
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-12-03 16:46:02 +0545

    Implement fetching data with useEffect Part3, V14

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import HookCounter from "./components/HookCounter"
 import EffectDataFetching from "./components/useEffectExamples/EffectDataFetching"
+import EffectDataFetching3 from "./components/useEffectExamples/EffectDataFetching3"
 import EffectHookCounterOne from "./components/useEffectExamples/EffectHookCounterOne"
 import EffectHookMouse from "./components/useEffectExamples/EffectHookMouse"
 import HookCounterFour from "./components/useStateExamples/HookCounterFour"
@@ -9,8 +10,7 @@ import HookCounterFour from "./components/useStateExamples/HookCounterFour"
 const App = () => {
   return (
     <div>
-        <EffectHookMouse />
-        <EffectDataFetching />
+       <EffectDataFetching3 />
     </div>
   )
 }
