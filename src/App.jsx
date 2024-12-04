346e6258c1a1e53eb8e7669770e11168c7ba600f
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-12-04 15:29:13 +0545

    useContext Hook Part 2, V16

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import React from 'react'
 // import HookCounter from "./components/HookCounter"
+import ComponentC from "./components/useContextExamples/ComponentC"
 import EffectDataFetching from "./components/useEffectExamples/EffectDataFetching"
 import EffectDataFetching3 from "./components/useEffectExamples/EffectDataFetching3"
 import EffectHookCounterOne from "./components/useEffectExamples/EffectHookCounterOne"
@@ -7,12 +9,19 @@ import HookCounterFour from "./components/useStateExamples/HookCounterFour"
 // import HookCounterThree from "./components/HookCounterThree"
 // import HookCounterTwo from "./components/HookCounterTwo"
 
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
 const App = () => {
   return (
     <div>
-       <EffectDataFetching />
+      <UserContext.Provider value={'Puskar'}>
+        <ChannelContext.Provider value={'Codevolution'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   )
 }
 
-export default App
+export default App 
