873e6964cb59c843fb7492d89d7d36a5d8f1e127
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-12-05 15:58:15 +0545

    Implemented useReducer(simple state & action); V19

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
-// import HookCounter from "./components/HookCounter"
-import ComponentC from "./components/useContextExamples/ComponentC"
-import EffectDataFetching from "./components/useEffectExamples/EffectDataFetching"
-import EffectDataFetching3 from "./components/useEffectExamples/EffectDataFetching3"
-import EffectHookCounterOne from "./components/useEffectExamples/EffectHookCounterOne"
-import EffectHookMouse from "./components/useEffectExamples/EffectHookMouse"
-import HookCounterFour from "./components/useStateExamples/HookCounterFour"
-// import HookCounterThree from "./components/HookCounterThree"
-// import HookCounterTwo from "./components/HookCounterTwo"
-
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
+import CounterOne from './components/useReducerExamples/CounterOne'
 
 const App = () => {
   return (
     <div>
-      <UserContext.Provider value={'Puskar'}>
-        <ChannelContext.Provider value={'Codevolution'}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+        <CounterOne />
     </div>
   )
 }
