4edb00972a3415c270767d61d9564918bbd68b00
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-12-06 13:52:02 +0545

    Basic structure created for useCallback Hook example, V26

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import DataFetchingOne from './components/useReducerExamples/DataFetchingOne'
 import DataFetchingTwo from './components/useReducerExamples/DataFetchingTwo'
+import ParentComponent from './components/useCallbackExamples/ParentComponent'
 
 const App = () => {
   return (
     <div>
-        <DataFetchingTwo />
+        <ParentComponent />
     </div>
   )
 }
