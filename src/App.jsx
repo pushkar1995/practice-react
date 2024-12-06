39cf085737d56531c16645a421843aab87b7389d
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-12-06 11:11:25 +0545

    Implemented Fetching data with useReducer Part2, V24

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import DataFetchingOne from './components/useReducerExamples/DataFetchingOne'
+import DataFetchingTwo from './components/useReducerExamples/DataFetchingTwo'
 
 const App = () => {
   return (
     <div>
-        <DataFetchingOne />
+        <DataFetchingTwo />
     </div>
   )
 }
