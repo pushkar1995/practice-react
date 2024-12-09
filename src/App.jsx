f5eddca3b135d90702b8d33ec62024846f5e0cb9
pushkar gautam <pushkar.gautam1994@gmail.com>
2024-12-09 21:10:48 +0545

    Implemented useRef Hook Part1,V28

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Counter from './components/useMemoExamples/Counter'
+import FocusInput from './components/useRefExamples/FocusInput'
+
 
 const App = () => {
   return (
     <div>
-        <Counter />
+      <FocusInput />
     </div>
   )
 }
